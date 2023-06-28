ede12a75fc6793a948d3ea2e8688988845042854
User <128024046+BCUrepo@users.noreply.github.com>
2023-06-28 13:21:14 +0530

    Add files via upload

---
+++ Assertions.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Assertions {
+
+    public static void assertNotNull(List<Booking> saturdayBookings) {
+    }
+
+    public static void assertEquals(String string, String string2) {
+    }
+
+    public static void assertEquals(String string, String name) {
+    }
+
+    public static void assertEquals(int i, int size) {
+    }
+
+    public static void assertFalse(boolean hasAttended) {
+    }
+
+}
